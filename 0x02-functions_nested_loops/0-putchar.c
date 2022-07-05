a8eb4efe218a93508a597abec690f62ec5a75f8c
Ashaolu Peter O <pyrotech.sv@gmail.com>
2022-07-05 13:11:08 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - entry point
  *
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
-printf('_');
-printf('p');
-printf('u');
-printf('t');
-printf('c');
-printf('h');
-printf('a');
-printf('r');
-printf('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-return (0);
+	return (0);
 }
+
