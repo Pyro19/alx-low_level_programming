0d5e5e0d35dfab92263a31d1ced53b6fd5a7b582
Ashaolu Peter O <pyrotech.sv@gmail.com>
2022-07-07 12:49:41 -0700

    b

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test function that prints if integer is positive of negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	printf("%d is positive\n", i);
+
+	return (0);
+}
