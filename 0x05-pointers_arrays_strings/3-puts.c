6c51d048b32c0ed0b2fabc2950bc68ff0ee18ea9
Ashaolu Peter O <pyrotech.sv@gmail.com>
2022-07-12 07:56:50 -0700

    print string with a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - function thst print sting
+ * @str: string number
+ * Return: string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
