bd58311909b5ba280108505e70afb652522c5f5c
Ashaolu Peter O <pyrotech.sv@gmail.com>
2022-07-12 21:50:09 -0700

    print other char of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
  * puts2 - functions tyat print every other char of a string
- * @str: input
- * @s: input
+ * @str: str to be created
  * return: string
  */
 void puts2(char *str)
 {
 	int i;
-	int j = 0;
+	int  j = 0;
 
 	while (str[j] != '\0')
 	{
