67d40a2a3be1d6844e47480a4b8b3a9a9319dd48
Ashaolu Peter O <pyrotech.sv@gmail.com>
2022-07-14 04:53:11 -0700

    concernate of 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - function that concernate 2 string
+ * @src: append string
+ * @dest: string to append to
+ * Return: concernate str
+ */
+char *_strcat(char *dest, char *src);
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0 ; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+	dest[dlen] = '\0';
+	return (dest);
+}
