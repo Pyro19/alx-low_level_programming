13e7b16ec94d9d8b59ba160b7208b213cd8a871c
Ashaolu Peter O <pyrotech.sv@gmail.com>
2022-07-14 11:25:15 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - function thst copy a string
+ * @dest:  first param
+ * @src: secom param
+ * @n: third param
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	retun(dest);
+}
