f27a1ad935aa210f54d0d1b8bfcf56ec2a462c9c
Ashaolu Peter O <pyrotech.sv@gmail.com>
2022-07-18 14:51:20 -0700

    code that get lenght

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - getting the lenght of a prefix substring
+ * @s: source str
+ * @accept: accepted sring
+ * Return: return bytes in the initial segment
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a = 0, b, t = 0;
+
+	while (accepted[a])
+	{
+		b = 0;
+
+		while (s[b] != 32)
+		{
+			if (accepted[a] == s[b])
+			{
+				t++;
+			}
+			b++;
+		}
+		a++;
+	}
+
+	return (t);
+}
