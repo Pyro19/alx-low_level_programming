37339cb6a2246773fb57f318ca9e12fb4d083d91
Ashaolu Peter O <pyrotech.sv@gmail.com>
2022-07-18 12:47:21 -0700

    string character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - function that locate a character of string
+ * @c: input char
+ * @s: str character
+ * Return: the str from character found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return (NULL);
+}
