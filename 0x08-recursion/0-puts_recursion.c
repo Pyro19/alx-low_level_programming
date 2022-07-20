910a224bb2d8abd77fd7721b40eef9ee51d42375
Ashaolu Peter O <pyrotech.sv@gmail.com>
2022-07-20 07:17:11 -0700

    string by new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#inlude "main.h"
+/**
+ * _puts_recursion - function that print a strimg, followed by a new line
+ * @s: input string
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++
+		_puts_recursion(s);
+}
