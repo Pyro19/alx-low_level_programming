e936b6db0628ca61ef3a4a1361dcaf55ba0a003a
Ashaolu Peter O <pyrotech.sv@gmail.com>
2022-07-20 07:27:18 -0700

    reverse printing

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function that print in reverse
+ * @s: input sting
+ * Return: Always 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
