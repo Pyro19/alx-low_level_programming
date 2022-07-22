cab3aac63c01b482599b7007c88cd050823cb2c3
Ashaolu Peter O <pyrotech.sv@gmail.com>
2022-07-22 09:07:10 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print its name
+ * @argv: array pointing to argument
+ * @argc: argument count
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+
+	return (0);
+}
+
