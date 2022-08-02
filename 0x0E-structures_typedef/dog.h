4801b24a266aa3c3715218b447d8f9fb74e97e7b
Ashaolu Peter O <pyrotech.sv@gmail.com>
2022-08-02 09:04:05 -0700

    dog main

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,13 +7,19 @@
  * @name: dog name
  * @owner: dog owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-dog_t;
+};
+
+/**
+ * dog_t - Typedef for dog structure
+ */
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
